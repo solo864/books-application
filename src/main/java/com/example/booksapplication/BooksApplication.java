bb46988ce99b492b2f16c4b6d281d933eeefe7d7
iamusayev <iamusayev1337@gmail.com>
2022-05-16 19:59:40 +0400

    added application runner

---
+++ src/main/java/com/example/booksapplication/BooksApplication.java
@@ -0,0 +1,37 @@
+package com.example.booksapplication;
+
+import com.example.booksapplication.dao.entity.UserEntity;
+import com.example.booksapplication.dao.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.Set;
+
+
+@SpringBootApplication
+public class BooksApplication  {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksApplication.class, args);
+
+    }
+
+    @Bean
+    PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
+
