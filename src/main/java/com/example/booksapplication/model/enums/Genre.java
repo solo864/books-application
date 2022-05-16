54413acab14e45fb4be4d346f63e6a4d96e2f0c9
iamusayev <iamusayev1337@gmail.com>
2022-05-16 19:54:35 +0400

    added enums

---
+++ src/main/java/com/example/booksapplication/model/enums/Genre.java
@@ -0,0 +1,21 @@
+package com.example.booksapplication.model.enums;
+
+public enum Genre {
+    TRAVEL,
+    FANTASY,
+    STORY,
+    SHORT_STORY,
+    DETECTIVE_STORY,
+    LOVE_STORY,
+    HORROR_STORY,
+    FAIRY_TALE,
+    LEGEND,
+    HISTORY,
+    SELF_DEVELOPMENT,
+    COMPUTER_SCIENCE,
+    PROGRAMMING,
+    SPIRITUAL,
+    BUSINESS,
+    PSYCHOLOGY,
+    NON_FICTION
+}
