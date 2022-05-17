0b6d214e63374150fb44365d45cffe51bc195dd1
iamusayev <iamusayev1337@gmail.com>
2022-05-17 10:41:42 +0400

    Added authentication handlers

---
+++ src/main/java/com/example/booksapplication/security/handler/SimpleAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package com.example.booksapplication.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+public class SimpleAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        HashMap<String, String> map = new HashMap<>(2);
+        map.put("uri", request.getRequestURI());
+        map.put("msg", "Having problems with authentication");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setCharacterEncoding("utf-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String resBody = objectMapper.writeValueAsString(map);
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print(resBody);
+        printWriter.flush();
+        printWriter.close();
+    }
+}
+
---
+++ src/main/java/com/example/booksapplication/security/handler/SimpleAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.example.booksapplication.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+public class SimpleAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        HashMap<String, String> map = new HashMap<>(2);
+        map.put("uri", request.getRequestURI());
+        map.put("msg", "Having problems with authentication");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setCharacterEncoding("utf-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String resBody = objectMapper.writeValueAsString(map);
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print(resBody);
+        printWriter.flush();
+        printWriter.close();
+    }
+}
