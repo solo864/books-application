b936e3da9b3e7c4eacdaa3bbe4e219ee10e08b5f
Anar Musayev <anarmusayev@digirella.az>
2022-07-27 20:27:59 +0400

    Added AuthorRepository

--- src/main/java/com/example/booksapplication/dao/repository/AuthorRepository.java
+++ src/main/java/com/example/booksapplication/dao/repository/AuthorRepository.java
@@ -4,5 +4,6 @@ import com.example.booksapplication.dao.entity.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+
 public interface AuthorRepository extends JpaRepository<AuthorEntity,Long> {
 }
